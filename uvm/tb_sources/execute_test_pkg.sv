6aa04d0ca6cffba22a22febf1cc3bae02935c22a
m3n2jz <russell.klein@siemens.com>
2025-03-02 17:05:54 -0800

    missed files

---
+++ uvm/tb_sources/execute_test_pkg.sv
@@ -0,0 +1,25 @@
+package riscv_test_pkg;
+
+  import opcodes::*;
+  import uvm_pkg::*;
+
+  `include "uvm_macros.svh"
+
+  `include "exec_record.svh"
+  `include "iss_class.svh"
+
+  `include "riscv_monitor.svh"
+  `include "riscv_tracer.svh"
+  `include "riscv_scoreboard.svh"
+
+  `include "execute_driver.svh"
+  `include "execute_agent.svh"
+  `include "execute_env.svh"
+  `include "execute_test.svh"
+
+  // tests derived from execute_test
+
+  `include "mem_test.svh"
+  `include "fibonacci.svh"
+  
+endpackage
